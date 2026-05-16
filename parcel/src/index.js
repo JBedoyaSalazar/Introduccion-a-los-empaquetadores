7136edde51e13fe63e0a28b957dbf1d2847ba7d6
Jefred Bedoya <jefred03salazar@gmail.com>
2026-05-15 21:26:10 -0500

    feat: initialize project with Parcel bundler, HTML structure, and JS product fetching logic

--- parcel/src/index.js
+++ parcel/src/index.js
@@ -1,2 +1,28 @@
+import './styles.css';
+
 const API_URL = "https://api.escuelajs.co/api/v1/products?offset=0&limit=10";
 const $app = document.getElementById('app');
+
+const main = async () => {
+    const response = await fetch(API_URL);
+    const products = await response.json();
+
+    const view = `
+    <div class="Items">
+        ${products.map(product => {
+            return `
+                <div class="Card">
+                    <img src="${product.images[0]}" alt="${product.title}">
+                    <h2>
+                        <a href="#${product.id}">${product.title}</a>
+                        <span>$${product.price}</span>
+                    </h2>
+                </div>
+            `
+        }).join('')}
+    </div>`;
+
+    $app.innerHTML = view;
+}
+
+main();
